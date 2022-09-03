5b6ffe64971be999e8fbf61d9806acd493d55c50
Szymon Urbaś <szmn@urbs.me>
2022-09-04 00:43:26 +0200

    only process the part of frame where the boss bar might be

---
+++ game_config.py
@@ -0,0 +1,8 @@
+GAME_CONFIG = {
+  "bloodborne": {
+    "boss_bar_y_start_pct": 0.815,
+    "boss_bar_y_end_pct": 0.885,
+    "boss_bar_x_start_pct": 0.164,
+    "boss_bar_x_end_pct": 0.835,
+  }
+}
