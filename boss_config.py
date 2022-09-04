19e8b22fd8cb4daaccae42b1cffb0147d306eb68
Szymon Urbaś <szmn@urbs.me>
2022-09-04 14:32:23 +0200

    add few more bosses

--- boss_config.py
+++ boss_config.py
@@ -1,7 +1,23 @@
 BOSS_CONFIG = {
+  "amygdala": {
+    "game": "bloodborne",
+    "boss_name_tmpl": "amygdala.png",
+  },
+  "cleric_beast": {
+    "game": "bloodborne",
+    "boss_name_tmpl": "cleric_beast.png",
+  },
   "ebrietas": {
     "game": "bloodborne",
     "boss_name_tmpl": "ebrietas.png",
+  },
+  "emissary": {
+    "game": "bloodborne",
+    "boss_name_tmpl": "emissary.png",
+  },
+  "logarius": {
+    "game": "bloodborne",
+    "boss_name_tmpl": "logarius.png",
   },
   "ludwig": {
     "game": "bloodborne",
@@ -10,5 +26,9 @@ BOSS_CONFIG = {
   "mergos": {
     "game": "bloodborne",
     "boss_name_tmpl": "mergos_wet_nurse.png",
+  },
+  "the_one_reborn": {
+    "game": "bloodborne",
+    "boss_name_tmpl": "the_one_reborn.png",
   }
 }
